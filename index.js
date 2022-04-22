9f99de972bd67248dc97b5be6c40f1091bfde538
Owoade <63651250+Owoade@users.noreply.github.com>
2022-04-22 21:57:07 +0000

    make it better

---
+++ index.js
@@ -0,0 +1,11 @@
+import Sequelize from "sequelize";
+import {config} from "dotenv";
+import express from "express";
+
+const app = express();
+const PORT = process.env.PORT || 3000
+config()
+
+const db = new Sequelize(process.env.DB_CONNECTION_STRING);
+db.authenticate()
+.then(app.listen(PORT,()=> console.log("DB and server running ")))
