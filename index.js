2b40f7e728a3ac70fbb54ede3e2619b76996fdcf
Owoade <63651250+Owoade@users.noreply.github.com>
2022-04-22 23:43:35 +0000

    new

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ import {config} from "dotenv";
 import express from "express";
 
 const app = express();
-const PORT = process.env.PORT || 3000
 config()
+const PORT = process.env.PORT || 3000
+
 
 app.get("/",(req,res)=>{
     console.log("Anticipa")
@@ -12,5 +13,8 @@ app.get("/",(req,res)=>{
 const db = new Sequelize(process.env.DATABASE_URL,{
     dialect:"postgres"
 });
+const User = db.define("user",{
+    name:Sequelize.STRING
+})
 
 app.listen(PORT,()=> console.log("DB and server running "))
