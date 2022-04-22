209044edba81e3e5e19180600284a070f4a038f4
Owoade <63651250+Owoade@users.noreply.github.com>
2022-04-22 23:10:32 +0000

    changed env variables

--- index.js
+++ index.js
@@ -7,11 +7,10 @@ const PORT = process.env.PORT || 3000
 config()
 
 app.get("/",(req,res)=>{
-    console.log("Anticipate")
+    console.log("Anticipa")
 })
-const db = new Sequelize('de2tehpiej95dn', 'tyuspqwaarrzgb', '61470c48741495322929045749008883970be6bd7512757bd752d9060ef5c257', {
-    host: 'ec2-44-199-143-43.compute-1.amazonaws.com',
-    dialect:  'postgres' 
-  })
+const db = new Sequelize(process.env.DATABASE_URL,{
+    dialect:"postgres"
+});
 db.authenticate()
 .then(app.listen(PORT,()=> console.log("DB and server running ")))
