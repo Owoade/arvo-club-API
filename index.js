0c03dff9f529b404f3f4a27b3dcb354ced1d5d27
Owoade <63651250+Owoade@users.noreply.github.com>
2022-04-22 22:33:23 +0000

    added route

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const app = express();
 const PORT = process.env.PORT || 3000
 config()
 
+app.get("/",(req,res)=>{
+    console.log("Anticipate")
+})
 const db = new Sequelize(process.env.DB_CONNECTION_STRING);
 db.authenticate()
 .then(app.listen(PORT,()=> console.log("DB and server running ")))
